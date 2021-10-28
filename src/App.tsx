0a8605c7d653000e5587f9dcfcaf484ee789b373
elliotreify <elliot.stern@reifyhealth.com>
2021-10-28 13:38:04 -0400

    inital version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type MenuItem = {
+    name: string;
+    description: string;
+    price: number;
 }
+const menu: Array<MenuItem> = [
+    {name: "Chicken Tikka Masala", description: "Tandoori chicken in tomato gravy", price: 15.5}, 
+    {name: "Aloo Gobi", description: "spiced cauliflower potatoes", price: 12.5}
+];
 
-export default App;
+export function App() {
+    return <> {/* commenty comment */}
+            <div> 
+              <h1 style={{color:"blue"}}> Entree </h1>
+              <p> Welcome! </p>
+                <ul>
+                {menu.map(item => <li> {item.name} </li>)}
+                </ul>
+            </div>
+           </>;
+}
